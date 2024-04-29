80aa574f384ef3a31fecd525d9235a151b3c401d
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-04-29 11:11:15 -0400

    updated

--- main.js
+++ main.js
@@ -2,11 +2,17 @@
 // 29 APR 2024
 // Array Basics
 
-const firstName = "Katelyn";
-
 // Input
+const firstName = 'Katelyn';
+const lastName =  'Curtiss';
+const date = '29 APR 2024';
+const title = 'Array Basics';
+
 const characters = ["Hello Kitty", "Pompompurn", "Cinnomenroll", "My Melody"];
 characters.unshift("Kurropi"); 
+
+const cites = ["Kyoto", "London", "Edinburgh", "Dublin"];
+characters.push("Kurropi"); 
 
 
 // Process
@@ -14,7 +20,14 @@ characters.unshift("Kurropi");
 
 // Output
 const myOutput = `
+${firstName} 
+${lastName}
+${date}
+${title}
+${characters}
+${cites}`;
 
-`
+console.log(myOutput);
 
-console.log(myoutput);
+
+
