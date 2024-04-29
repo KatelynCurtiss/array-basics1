dd872d42b5bceaa31c9ca94ecabb60207e3b3bbf
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-04-29 10:53:02 -0400

    updated new array basics page

---
+++ main.js
@@ -0,0 +1,20 @@
+// Katelyn Curtiss
+// 29 APR 2024
+// Array Basics
+
+const firstName = "Katelyn";
+
+// Input
+const characters = ["Hello Kitty", "Pompompurn", "Cinnomenroll", "My Melody"];
+characters.unshift("Kurropi"); 
+
+
+// Process
+
+
+// Output
+const myOutput = `
+
+`
+
+console.log(myoutput);
